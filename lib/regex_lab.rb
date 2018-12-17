b5043b2904257ebae499813400d3227c339c5dff
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2018-12-17 06:08:06 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,7 +5,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/(\Aun]|ing$)/)
+  text.scan(/(\Aun|ing$)/)
 end
 
 def words_five_letters_long(text)
